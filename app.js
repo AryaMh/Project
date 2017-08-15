95cee4bcfbbeb22268d464600a1d2a503f00e1f2
saeid2510 <saeid2510@gmail.com>
2017-08-15 22:44:18 +0430

    create our routes in app folder create our views in views folder update package.json update app.js

--- app.js
+++ app.js
@@ -2,17 +2,35 @@ var express = require('express');
 var dbhandler = require('./db/dbhandler.js');
 var app = express();
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
 
-app.get('/', function (req, res) {
-    res.send("Testing");
-});
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+// app.get('/', function (req, res) {
+//     res.send("Testing");
+// });
 
 dbhandler.initial();
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 var StudentModel = dbhandler.getStudentModel();
 dbhandler.add("Pardis","Pardisan", "91123456",['kir', 'kir']);
 dbhandler.add("Farbod","Farbodan", "92344923", ['kir', 'kir']);
-
 
 app.use(express.static(process.cwd()+"/public"));
 app.use(bodyParser.urlencoded( {extended: true} ));
@@ -40,6 +58,7 @@ app.post('/students/add', function(req, res) {
     dbhandler.add(req.body.firstName, req.body.lastName, req.body.studentID);
 });
 
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 app.listen(3000);
 console.log("Listening on port *: 3000");
