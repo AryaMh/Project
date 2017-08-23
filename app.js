b962f9f5dee25737d19cbc5f963af20b24de4356
hvdh.ts@gmail.com <Aryamh123>
2017-08-23 17:51:52 +0430

    crawling done.

--- app.js
+++ app.js
@@ -1,66 +1,33 @@
 var express = require('express');
 var dbhandler = require('./db/dbhandler.js');
+var crawler = require('./db/crawler');
 var app = express();
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var flash = require('connect-flash');
-
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-
-// app.get('/', function (req, res) {
-//     res.send("Testing");
-// });
+require('./config/passport')(passport); //pass passport for configuration
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 dbhandler.initial();
-
-require('./config/passport')(passport); //pass passport for configuration
+crawler.initial();
+crawler.getCPS();
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
-
 app.set('view engine', 'ejs'); // set up ejs for templating
-
 // required for passport
 app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
-
-var StudentModel = dbhandler.getStudentModel();
-dbhandler.add("Pardis","Pardisan", "91123456",['kir', 'kir']);
-dbhandler.add("Farbod","Farbodan", "92344923", ['kir', 'kir']);
-
 app.use(express.static(process.cwd()+"/public"));
 app.use(bodyParser.urlencoded( {extended: true} ));
 app.use(bodyParser.json());
-
-app.get('/students', function(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1333');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    StudentModel.find({}, function(error, data) {
-        if (data)
-            res.json(data);
-    });
-});
-
-app.post('/students/add', function(req, res) {
-    dbhandler.add(req.body.firstName, req.body.lastName, req.body.studentID);
-});
-
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 app.listen(3000);
 console.log("Listening on port *: 3000");
---
+++ db/crawler.js
@@ -0,0 +1,38 @@
+/**
+ * Created by ARYA on 8/23/2017.
+ */
+var crawlJson = require('./crawl.json');
+
+var mongoose = require('mongoose');
+
+var MongoServerIP = "127.0.0.1";
+var DBName = "University";
+
+var CourseProfessorStructure = {
+    ProfessorName: String,
+    ProfessorEmail: String,
+    CourseName: String
+};
+
+var Schema = new mongoose.Schema(CourseProfessorStructure);
+var CPModel = mongoose.model("CourseProfessors", Schema);
+
+
+module.exports.initial = function (){
+    mongoose.connect("mongodb://"+MongoServerIP+"/"+DBName,{
+        useMongoClient: true
+        /* other options */
+    });
+    CPModel.remove({}, function (err) {} );
+};
+
+module.exports.getCPS = function() {
+    for(var i = 0 ; i < crawlJson.CP.length; i++){
+        new CPModel({ProfessorName: crawlJson.CP[i].ProfessorName, ProfessorEmail: crawlJson.CP[i].ProfessorEmail, CourseName: crawlJson.CP[i].CourseName}).save();
+    }
+    //console.log(crawlJson[0]);
+};
+
+module.exports.deleteAll = function(){
+    CPModel.remove({}, function (err) {} );
+};
